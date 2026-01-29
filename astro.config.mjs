246b2dd5a3f54718f9652f43707b464567c72d2e
siqjulia <jusiqueira@davidson.edu>
2026-01-29 17:32:35 -0500

    Added post-1.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
 
 export default defineConfig({
   site: 'https://siqjulia.github.io', // DOMAIN
   base: '/astroblog', // REPOSITORY
   output: 'static',
-  integrations: [svelte()]
 });
