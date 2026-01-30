83f68d19812816d0f75efbd9d18a157789601ad0
siqjulia <jusiqueira@davidson.edu>
2026-01-30 12:04:31 -0500

    Removed base and added site only

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://siqjulia.github.io', // DOMAIN
-  base: '/astroblog', // REPOSITORY
+  site: 'https://astroblog-jsiq.netlify.app/', // DOMAIN
   output: 'static',
 });
